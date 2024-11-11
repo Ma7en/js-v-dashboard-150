6831726d53f6bfe83f62ae0c1df861b1dba854ce
Ma7en <m9ee9m@gmail.com>
2024-11-11 12:17:06 +0200

    edit prettier

--- assets/js/pages/dashboard.init.js
+++ assets/js/pages/dashboard.init.js
@@ -1,5 +1,5 @@
-setTimeout(function() {
-    $("#subscribeModal").modal("show")
+setTimeout(function () {
+    $("#subscribeModal").modal("show");
 }, 2e3);
 var options = {
         chart: {
@@ -7,50 +7,57 @@ var options = {
             type: "bar",
             stacked: !0,
             toolbar: {
-                show: !1
+                show: !1,
             },
             zoom: {
-                enabled: !0
-            }
+                enabled: !0,
+            },
         },
         plotOptions: {
             bar: {
                 horizontal: !1,
                 columnWidth: "20px",
-                endingShape: "rounded"
-            }
+                endingShape: "rounded",
+            },
         },
         dataLabels: {
-            enabled: !1
+            enabled: !1,
         },
-        series: [{
-            name: "Series A",
-            data: [44, 55, 41, 67, 22, 43, 36]
-        }, {
-            name: "Series B",
-            data: [13, 23, 20, 8, 13, 27, 18]
-        }, {
-            name: "Series C",
-            data: [11, 17, 15, 15, 21, 14, 11]
-        }],
+        series: [
+            {
+                name: "Series A",
+                data: [44, 55, 41, 67, 22, 43, 36],
+            },
+            {
+                name: "Series B",
+                data: [13, 23, 20, 8, 13, 27, 18],
+            },
+            {
+                name: "Series C",
+                data: [11, 17, 15, 15, 21, 14, 11],
+            },
+        ],
         xaxis: {
-            categories: ["Sat", "Sun", "Mon", "Tuh", "Wed", "Thu", "Fri"]
+            categories: ["Sat", "Sun", "Mon", "Tuh", "Wed", "Thu", "Fri"],
         },
         colors: ["#556ee6", "#f1b44c", "#34c38f"],
         legend: {
-            position: "bottom"
+            position: "bottom",
         },
         fill: {
-            opacity: 1
-        }
+            opacity: 1,
+        },
     },
-    chart = new ApexCharts(document.querySelector("#stacked-column-chart"), options);
+    chart = new ApexCharts(
+        document.querySelector("#stacked-column-chart"),
+        options
+    );
 chart.render();
 options = {
     chart: {
         height: 200,
         type: "radialBar",
-        offsetY: -10
+        offsetY: -10,
     },
     plotOptions: {
         radialBar: {
@@ -60,35 +67,38 @@ options = {
                 name: {
                     fontSize: "13px",
                     color: void 0,
-                    offsetY: 60
+                    offsetY: 60,
                 },
                 value: {
                     offsetY: 22,
                     fontSize: "16px",
                     color: void 0,
-                    formatter: function(e) {
-                        return e + "%"
-                    }
-                }
-            }
-        }
+                    formatter: function (e) {
+                        return e + "%";
+                    },
+                },
+            },
+        },
     },
     colors: ["#556ee6"],
     fill: {
         type: "gradient",
         gradient: {
             shade: "dark",
-            shadeIntensity: .15,
+            shadeIntensity: 0.15,
             inverseColors: !1,
             opacityFrom: 1,
             opacityTo: 1,
-            stops: [0, 50, 65, 91]
-        }
+            stops: [0, 50, 65, 91],
+        },
     },
     stroke: {
-        dashArray: 4
+        dashArray: 4,
     },
     series: [67],
-    labels: ["Series A"]
+    labels: ["Series A"],
 };
-(chart = new ApexCharts(document.querySelector("#radialBar-chart"), options)).render();
+(chart = new ApexCharts(
+    document.querySelector("#radialBar-chart"),
+    options
+)).render();
